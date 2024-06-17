dc9ec1c21b08db86694fdd51a9ae6c9d2c501afb
jamie <j2clark@gmail.com>
2024-06-17 11:28:49 -0700

    add s3 read

--- code/setup.py
+++ code/setup.py
@@ -10,9 +10,21 @@ setup(
         'pyshell'
     ],
     install_requires=[
+        # TODO: add packages locally to ensure a fixed known environment
         'watchtower==3.0.1',
         'pendulum==2.1.2',
         'pandas==2.2.1',
+        # 'python-dateutil>=2.8.2',
+        # 'tzdata>=2022.7',
+        # 'pytz>=2020.1',
+        # 'numpy<2,>=1.22.4',
+        # 'pytzdata>=2020.1',
+        # 'boto3<2,>=1.9.253',
+        # 'jmespath<2.0.0,>=0.7.1',
+        # 's3transfer<0.11.0,>=0.10.0',
+        # 'botocore<1.35.0,>=1.34.127',
+        # 'six>=1.5',
+        # 'urllib3<1.27,>=1.25.4'
     ],
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
--- code/src/pyshell/pybatch.py
+++ code/src/pyshell/pybatch.py
@@ -1,3 +1,6 @@
+import boto3.session
+
+
 class PyBatch(object):
 
     def __init__(self, raw_args: list[str]):
@@ -5,8 +8,6 @@ class PyBatch(object):
 
         print('raw_args: ' + str(raw_args))
 
-        # count = len(raw_args)
-        # skip first parameter
         index = 0
         while index < len(raw_args):
             arg = raw_args[index]
@@ -29,6 +30,11 @@ class PyBatch(object):
         print('hello world!')
 
         print(f'read from S3 here: S3://{self.bucket}/{self.input}')
+        session = boto3.session.Session(region_name='us-west-1')
+        s3_client = session.client('s3')
+        data_input = s3_client.get_object(Bucket=self.bucket, Key=self.input)
+        body = data_input['Body'].read().decode('utf-8')
+        print('S3 Input: ' + body)
 
         print(f'write to S3 here: S3://{self.bucket}/{self.output}')
 
