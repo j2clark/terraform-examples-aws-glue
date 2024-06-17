3c83d2dee667828db6e85806cd8066cf1e1b6adf
jamie <j2clark@gmail.com>
2024-06-17 14:58:32 -0700

    add alarm back in

--- code/terraform/eventbridge.tf
+++ code/terraform/eventbridge.tf
@@ -1,28 +1,28 @@
-# resource "aws_cloudwatch_metric_alarm" "nlb_healthyhosts" {
-#   alarm_name          = "${local.name_prefix}-failed-gluejob"
-#   alarm_description   = "Number of failed jobs"
-#   statistic           = "Sum"
-#   comparison_operator = "GreaterThanOrEqualToThreshold"
-#   datapoints_to_alarm = 1
-#   # period is in seconds. 10, 30 or 60*n
-#   period              = "60"
-#   evaluation_periods  = "5"
-#
-#   namespace           = "examples-aws-glue"
-#   metric_name         = "GlueJobException"
-#
-#   dimensions = {
-#     GLUE_JOB = aws_glue_job.framework_job.name
-#     BRANCH = var.branch
-#   }
-#
-#   #   actions_enabled     = "true"
-# #   alarm_actions       = [
-# #     aws_sns_topic.alarm.arn
-# #   ]
-#
-#   tags = local.common_tags
-# }
+resource "aws_cloudwatch_metric_alarm" "nlb_healthyhosts" {
+  alarm_name          = "${local.name_prefix}-failed-gluejob"
+  alarm_description   = "Number of failed jobs"
+  statistic           = "Sum"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  datapoints_to_alarm = 1
+  # period is in seconds. 10, 30 or 60*n
+  period              = "60"
+  evaluation_periods  = "5"
+
+  namespace           = "examples-aws-glue"
+  metric_name         = "GlueJobException"
+
+  dimensions = {
+    GLUE_JOB = aws_glue_job.framework_job.name
+    BRANCH = var.branch
+  }
+
+  #   actions_enabled     = "true"
+#   alarm_actions       = [
+#     aws_sns_topic.alarm.arn
+#   ]
+
+  tags = local.common_tags
+}
 
 # resource "aws_cloudwatch_event_target" "sns" {
 #   rule      = aws_cloudwatch_event_rule.console.name
--- infrastructure/iam_codebuild_policy.tf
+++ infrastructure/iam_codebuild_policy.tf
@@ -77,6 +77,7 @@ data "aws_iam_policy_document" "codebuild_policy_document" {
     actions = [
       "cloudwatch:DescribeAlarms",
       "cloudwatch:ListTagsForResource",
+      "cloudwatch:DeleteAlarms",
     ]
     resources = [
       "arn:aws:cloudwatch:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:alarm:${local.name_prefix}-*"
